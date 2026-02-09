e1ec3c0348029348b8c4a64492073ca508871b09
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-09 22:49:29 +0800

    Add captcha config

--- config/captcha.php
+++ config/captcha.php
@@ -1,6 +1,11 @@
 <?php
 
 return [
+    // Login/General CAPTCHA
     'sitekey' => env('CLOUDFLARE_SITEKEY'),
     'secret' => env('CLOUDFLARE_SECRET_KEY'),
+
+    // Submit Tip CAPTCHA
+    'submit_tip_sitekey' => env('SUBMIT_TIP_CLOUDFLARE_SITEKEY'),
+    'submit_tip_secret' => env('SUBMIT_TIP_CLOUDFLARE_SECRET_KEY'),
 ];
