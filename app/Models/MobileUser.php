82a6050568ddc011fa4fa169cafae0890238642e
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-10 15:43:23 +0800

    Add jtw subject

--- app/Models/MobileUser.php
+++ app/Models/MobileUser.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class MobileUser extends Model
+class MobileUser extends Model implements JWTSubject
 {
     use HasFactory;
 
@@ -20,4 +21,24 @@ class MobileUser extends Model
     protected $hidden = [
         'password_hash',
     ];
+
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
 }
