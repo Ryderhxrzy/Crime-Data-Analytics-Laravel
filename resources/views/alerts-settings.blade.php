b4afffba5c126d26a6b2d5f2271ea2e36c573c12
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-16 22:03:15 +0800

    create alerts settings

---
+++ resources/views/alerts-settings.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="flex-1 overflow-auto">
+    <div class="max-w-7xl mx-auto px-4 py-8">
+        <h1 class="text-2xl font-bold">Threshold Settings</h1>
+    </div>
+</div>
+@endsection
