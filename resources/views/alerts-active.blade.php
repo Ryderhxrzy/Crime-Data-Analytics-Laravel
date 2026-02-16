0c7192ba19becc48111fc3f3632f953063b18967
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-16 22:03:00 +0800

    create laerts active

---
+++ resources/views/alerts-active.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="flex-1 overflow-auto">
+    <div class="max-w-7xl mx-auto px-4 py-8">
+        <h1 class="text-2xl font-bold">Active Alerts</h1>
+    </div>
+</div>
+@endsection
