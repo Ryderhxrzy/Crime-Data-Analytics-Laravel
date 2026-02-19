9da9aab281c09e6a3f2dfd8a9961690bf4437a3e
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-19 19:20:42 +0800

    add key

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    define: {
+        __PUSHER_KEY__: JSON.stringify(process.env.PUSHER_APP_KEY),
+        __PUSHER_CLUSTER__: JSON.stringify(process.env.PUSHER_APP_CLUSTER),
+        __PUSHER_HOST__: JSON.stringify(process.env.PUSHER_HOST),
+        __PUSHER_PORT__: JSON.stringify(process.env.PUSHER_PORT),
+        __PUSHER_SCHEME__: JSON.stringify(process.env.PUSHER_SCHEME),
+    },
     server: {
         // Use IPv4 localhost instead of IPv6 to avoid CSP issues
         host: '127.0.0.1',
