47b9c4bba4a3edd2a8add818f17161f2171cfa16
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2026-02-21 12:17:11 +0800

    add crime incident controller

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\CrimeIncidentController;
 use App\Http\Controllers\Api\MobileUserController;
 
 /*
