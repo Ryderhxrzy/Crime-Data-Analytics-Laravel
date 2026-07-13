769a35ef1a0d44391553d380817241802a0a854b
Jhayr Cervantes <jhayrcervantes0981@gmail.com>
2026-07-13 09:30:43 +0800

    fix

--- routes/web.php
+++ routes/web.php
@@ -56,6 +56,7 @@ Route::middleware('jwt.api')->group(function () {
     Route::get('/crime-hotspot', [DashboardController::class, 'crimeHotspot'])->name('crime-hotspot');
     Route::get('/risk-forecasting', [DashboardController::class, 'riskForecasting'])->name('risk-forecasting');
     Route::get('/pattern-detection', [DashboardController::class, 'patternDetection'])->name('pattern-detection');
+    Route::post('/pattern-detection/simulate', [DashboardController::class, 'simulatePatterns'])->name('pattern-detection.simulate');
     Route::get('/crimes', [CrimeIncidentController::class, 'index'])->name('crimes.index');
     Route::get('/crime-incident/create', [CrimeIncidentController::class, 'create'])->name('crime-incident.create');
     Route::post('/crime-incident', [CrimeIncidentController::class, 'store'])->name('crime-incident.store');
